4af862d98ec33acdc056d7e58061ddeadbf5642d
lifestealer001 <abrormurtazaqulov@gmail.com>
2022-10-21 17:51:45 +0500

    77

--- bin/list_index07.dart
+++ bin/list_index07.dart
@@ -6,4 +6,16 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1){
+    List x = list1;
+    int i = 0;
+    while(i<list1.length){
+    if(x[i]==0){
+    x[i]=false;
+    }i+=1;}
+    
+    return x;
+}
+void main() {
+    print(func([0,1,0,0,1,0,1,0]));
+}
