76ac64f46272b34490a29355f3b0bde009588018
lifestealer001 <abrormurtazaqulov@gmail.com>
2022-10-21 18:28:43 +0500

    64

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -6,4 +6,20 @@
     Returns:
         bool: return answer
 */
-void main() {}
+bool func(List list1){
+    int x = list1[0];
+    int i = 0;
+    int sum = 0;
+    while(i<list1.length){
+        if(x==list1[i]){
+            sum+=1;
+        }
+        i+=1;
+    }
+    return list1.length==sum;
+}
+void main() {
+    print(func([5,5,5,5,5,5]));
+}
+
+
--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -6,4 +6,12 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List x){
+    if(x[0]>x[x.length-1]){
+        return [x[0]];
+    }
+    return [x[x.length-1]];
+}
+void main() {
+    print(func([12,5,6,6,13]));
+}
